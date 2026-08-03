5d50a746bd4b33c16360d4d71d03e3b938e6a79b
njange <128843179+njange@users.noreply.github.com>
2026-08-03 23:57:52 +0300

    completed week1 CS50G

--- Flappy Bird/main.lua
+++ Flappy Bird/main.lua
@@ -7,7 +7,7 @@ require 'PipePair'
 
 require 'StateMachine'
 require 'states/BaseState'
-require 'states/CountDownState'
+require 'states/CountdownState'
 require 'states/PlayState'
 require 'states/ScoreState'
 require 'states/TitleScreenState'
@@ -64,7 +64,7 @@ function love.load()
 
   gStateMachine = StateMachine {
     ['title'] = function() return TitleScreenState() end,
-    ['countdown'] = function() return CountDownState() end,
+    ['countdown'] = function() return CountdownState() end,
     ['play'] = function() return PlayState() end,
     ['score'] = function() return ScoreState() end,
   }
