27290fb298be4bea88812d6792eb4256fa813db9
njange <128843179+njange@users.noreply.github.com>
2026-08-03 23:54:26 +0300

    flappy bird sstarts

--- Flappy Bird/Bird.lua
+++ Flappy Bird/Bird.lua
@@ -1,38 +1,40 @@
 Bird = Class{}
 
-local GRAVITY = 17
+local GRAVITY = 20
+local JUMP_FORCE = -5
 
 function Bird:init()
-    self.image = love.graphics.newImage('bird.png')
-    self.width = self.image:getWidth()
-    self.height = self.image:getHeight()
+  self.image = love.graphics.newImage('bird.png')
 
-    self.x = VIRTUAL_WIDTH / 2 - 8
-    self.y = VIRTUAL_HEIGHT / 2 - 8
+  self.width = self.image:getWidth()
+  self.height = self.image:getHeight()
 
+  self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
+  self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+
+  self.dy = 0
 end
 
 function Bird:collides(pipe)
-    if (self.x + 2) + (self.width - 4) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
-        if (self.y + 2) + (self.height - 4) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
-            return true
-        end
+  if (self.x + 2) + (self.width - 4) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
+    if (self.y + 2) + (self.height - 4) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
+      return true
     end
-
-    return false
+  end
+  return false
 end
 
 function Bird:update(dt)
-    self.dy = self.dy + GRAVITY * dt
+  self.dy = self.dy + GRAVITY * dt
 
-    if love.keyboard.wasPressed('space') then
-        self.dy = -5
-        gSounds['jump']:play()
-    end
+  if love.keyboard.wasPressed('space') or love.mouse.isDown(1) then
+    self.dy = JUMP_FORCE
+    sounds['jump']:play()
+  end
 
-    self.y = self.y + self.dy
+  self.y = self.y + self.dy
 end
 
 function Bird:render()
-    love.graphics.draw(self.image, self.x, self.y)
+  love.graphics.draw(self.image, self.x, self.y)
 end
--- Flappy Bird/main.lua
+++ Flappy Bird/main.lua
@@ -1,6 +1,16 @@
 push = require 'push'
 Class = require 'class'
+
 require 'Bird'
+require 'Pipe'
+require 'PipePair'
+
+require 'StateMachine'
+require 'states/BaseState'
+require 'states/CountDownState'
+require 'states/PlayState'
+require 'states/ScoreState'
+require 'states/TitleScreenState'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -8,54 +18,106 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
+local background = love.graphics.newImage('background.png')
 local backgroundScroll = 0
+
+local ground = love.graphics.newImage('ground.png')
 local groundScroll = 0
 
 local BACKGROUND_SCROLL_SPEED = 30
 local GROUND_SCROLL_SPEED = 60
 
 local BACKGROUND_LOOPING_POINT = 413
+local GROUND_LOOPING_POINT = 514
+
+local isPaused = false -- flag to pause game
 
 function love.load()
-    love.graphics.setDefaultFilter('nearest', 'nearest')
+  love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    background = love.graphics.newImage('background.png')
-    
-    ground = love.graphics.newImage('ground.png')
-    bird = Bird()
+  love.window.setTitle('Fifty Bird')
 
-    love.window.setTitle('Flappy Bird')
+  smallFont = love.graphics.newFont('font.ttf', 8)
+  mediumFont = love.graphics.newFont('flappy.ttf', 14)
+  flappyFont = love.graphics.newFont('flappy.ttf', 28)
+  hugeFont = love.graphics.newFont('flappy.ttf', 56)
+  love.graphics.setFont(flappyFont)
 
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-        vsync = true,
-        fullscreen = false,
-        resizable = true
-    })
+  sounds = {
+    ['jump'] = love.audio.newSource('jump.wav', 'static'),
+    ['explosion'] = love.audio.newSource('explosion.wav', 'static'),
+    ['hurt'] = love.audio.newSource('hurt.wav', 'static'),
+    ['score'] = love.audio.newSource('score.wav', 'static'),
+    ['music'] = love.audio.newSource('marios_way.mp3', 'static')
+  }
 
-    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
+  sounds['music']:setLooping(true)
+  sounds['music']:play()
+
+  math.randomseed(os.time())
+
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    vsync = true,
+    fullscreen = false,
+    resizable = true
+  })
+
+  gStateMachine = StateMachine {
+    ['title'] = function() return TitleScreenState() end,
+    ['countdown'] = function() return CountDownState() end,
+    ['play'] = function() return PlayState() end,
+    ['score'] = function() return ScoreState() end,
+  }
+  gStateMachine:change('title')
+
+  love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+  push:resize(w, h)
 end
 
 function love.keypressed(key)
-    if key == 'escape' then
-        love.event.quit()
+  love.keyboard.keysPressed[key] = true
+
+  if key == 'escape' then
+    love.event.quit()
+  end
+
+  if key == 'p' and gStateMachine.currentStateName == 'play' then
+    if isPaused then
+      isPaused = false
+    else
+      isPaused = true
     end
+  end
+end
+
+function love.keyboard.wasPressed(key)
+  return love.keyboard.keysPressed[key]
 end
 
 function love.update(dt)
+  if not isPaused then
     backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
-    
-    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
-    
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % GROUND_LOOPING_POINT
+
+    gStateMachine:update(dt)
+
+    love.keyboard.keysPressed = {}
+  end
 end
 
 function love.draw()
-    push.start()
+  push:start()
+  love.graphics.draw(background, -backgroundScroll, 0)
+  gStateMachine:render()
+  love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
-    love.graphics.draw(background, -backgroundScroll, 0)
+  if isPaused then
+    love.graphics.setFont(hugeFont)
+    love.graphics.printf('PAUSED!', 0, 120, VIRTUAL_WIDTH, 'center')
+  end
 
-    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
-    bird:render()
-
-    push.finish()
+  push:finish()
 end
-
